94288c44ce5786fc949795386a770c2462a3d74d
Momin huzaifa <mominhuzaifa7379@gmail.com>
2026-02-03 11:45:19 +0530

    BusinessERP

--- src/Pages/DashBoard/BusinessERP/BusinessERP.jsx
+++ src/Pages/DashBoard/BusinessERP/BusinessERP.jsx
@@ -1,82 +1,109 @@
-import React from 'react';
+import React from "react";
 
 export default function BusinessERP() {
   return (
-    <div className='content AssetsPageChangecss AssetPaddingChange'>
-      <div className="m-5">
-        <h2 className='fw-bolder'>Business ERP</h2>
-      </div>
-      <hr />
-      <div className="d-flex flex-wrap ps-5">
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/sale-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>New Sale</p>
+    <div className="content AssetsPageChangecss AssetPaddingChange">
+      <div className="marginforsmalldevice RemoveBorder rounded-0">
+        <div className="NewColorChange phoenix-toolbar d-flex align-items-center flex-wrap py-md-3 px-md-4 mb-md-0 border-bottom gap-3">
+          <div className="p-3 row MainRemovePaddingwidth">
+            <h2 className="fw-bolder mb-5" style={{ fontSize: "2rem" }}>
+              Business ERP
+            </h2>
           </div>
         </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/report-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Sales Report</p>
+        <div className="NewColorChange p-4 px-5">
+          <div className="row g-4">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/sale-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>New Sale</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/report-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Sales Report</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/customer-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Customer list</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/category-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Category List</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/stockk-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Manage Stock</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/hand-min.jpg" alt="" />
+                <div className="img-overlay text-center">
+                  <p>Supplier List</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/769-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Low Stock</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/config-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Config</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/Attendance-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Attendance Report</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/Purchase-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Purchase Report</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card h-100 shadow-sm text-center">
+                <img src="/src/assets/img/bg/summary-min.jpg" alt="" />
+                <div className="img-overlay">
+                  <p>Summary Report</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/customer-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Customer list</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/category-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Category List</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/stockk-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Manage Stock</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/hand-min.jpg" alt="" />
-          <div className="img-overlay text-center">
-            <p>Supplier List</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/769-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Low Stock</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/config-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Config</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/Attendance-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Attendance Report</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/Purchase-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Purchase Report</p>
-          </div>
-        </div>
-        <div className="card cardwidth ">
-          <img src="/src/assets/img/bg/summary-min.jpg" alt="" />
-          <div className="img-overlay">
-            <p>Summary Report</p>
-          </div>
-        </div>
-
-
       </div>
     </div>
-  )
+  );
 }
